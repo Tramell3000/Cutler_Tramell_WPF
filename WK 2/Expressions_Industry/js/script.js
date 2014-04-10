b687db23854a29b5b14a53d09bc4627ee618dfc5
TramellCutler <TramellCutler@fullsail.edu>
2014-04-10 13:00:13 -0400

    Commit 1

---
+++ WK 2/Expressions_Industry/js/script.js
@@ -0,0 +1,6 @@
+//photoshop U.S. Paper document size width and height in pixels
+ var pixelsInwidth = 2250 
+ var pixelsInheight = 3300 
+
+ console.log(pixelsInwidth);
+ console.log(pixelsInheight);

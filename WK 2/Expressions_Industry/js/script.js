2a37c7cdaf2b221edaa7b6cc7d5495fe2888c3e6
TramellCutler <TramellCutler@fullsail.edu>
2014-04-10 13:17:45 -0400

    commit 2
    Added commit

--- WK 2/Expressions_Industry/js/script.js
+++ WK 2/Expressions_Industry/js/script.js
@@ -1,6 +1,14 @@
 //photoshop U.S. Paper document size width and height in pixels
- var pixelsInwidth = 2250 
- var pixelsInheight = 3300 
+ var pixelsInwidth = 2250;
+ var pixelsInheight = 3300; 
 
  console.log(pixelsInwidth);
  console.log(pixelsInheight);
+
+
+//find the area of phtoshop web document
+
+var width = 11;
+var height = 8;
+var area = width * height
+console.log(area);

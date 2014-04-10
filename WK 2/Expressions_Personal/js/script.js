5bf60ff336754d012c3e0fb5095e88aac10d44b6
TramellCutler <TramellCutler@fullsail.edu>
2014-04-10 14:21:06 -0400

    commit
    first commit

--- WK 2/Expressions_Personal/js/script.js
+++ WK 2/Expressions_Personal/js/script.js
@@ -1,6 +1,8 @@
-
+//Pick choice of bodywash
 
 var bodyWash = prompt ("What kind of bodywash do you use?");
 
 console.log(bodyWash);
+
+alert("Irish Spring" +bodyWash+"!! How long have you been using it?");
 

13b6efed20c38ce3b1d65e0fd356d557b5fac25a
TramellCutler <TramellCutler@fullsail.edu>
2014-04-10 21:55:43 -0400

    commit
    change in expression

--- WK 2/Expressions_Wacky/js/script.js
+++ WK 2/Expressions_Wacky/js/script.js
@@ -3,6 +3,7 @@
 var carModel = "Plymouth Fury III";
 prompt("What is the model of your car?");
 console.log(carModel);
+
 var carYear = 1965;
 prompt("What year is the year of your car?");
  console.log(carYear);
@@ -10,3 +11,7 @@ prompt("What year is the year of your car?");
  var carColor = "Red"
  prompt("What color is your car?");
  console.log(carColor);
+
+ var carMake = "Dodge"
+ prompt("What is the make of your car?");
+ console.log(carMake);

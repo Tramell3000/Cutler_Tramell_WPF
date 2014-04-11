399dffb0bcacf78aeb0e367924b83d5dd9f6713f
TramellCutler <TramellCutler@fullsail.edu>
2014-04-10 21:46:17 -0400

    commit
    change of expression

--- WK 2/Expressions_Personal/js/script.js
+++ WK 2/Expressions_Personal/js/script.js
@@ -4,5 +4,10 @@ var bodyWash = prompt ("What kind of bodywash do you use?");
 
 console.log(bodyWash);
 
-alert("Irish Spring" +bodyWash+"!! How long have you been using it?");
+
+
+
+
+
+
 
--- WK 2/Expressions_Wacky/js/script.js
+++ WK 2/Expressions_Wacky/js/script.js
@@ -1,1 +1,8 @@
-alert("This is in the js file");
+//car information
+
+var carModel = "Plymouth Fury III";
+prompt("What is the model of your car?");
+console.log(carModel);
+var carYear = 1965;
+prompt("What year is the year of your car?");
+ console.log(carYear);

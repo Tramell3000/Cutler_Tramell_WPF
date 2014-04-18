b2695e5fa386f5f5d4ced22058813ce522b4313d
TramellCutler <TramellCutler@fullsail.edu>
2014-04-18 19:54:10 -0400

    commit
    change in commit

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,10 +1,11 @@
 var cash = 100000;
 var car = 60000;
 
-var cash = prompt(100000);
+
 
 if (cash > car){
 	console.log("You can purchase");
+
 
 }else{
 	console.log("You will not be able to purchase");
